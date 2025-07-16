3053e107d6d7c83759e478596ee8b49a3ad186a8
Gabriel BF <gbbolonhez@gmail.com>
2025-07-15 23:50:37 -0300

    first

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express, {Request, Response} from 'express';
+import createApp from './app'; 
+
+const port = process.env.PORT;
+
+const app = createApp();
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
